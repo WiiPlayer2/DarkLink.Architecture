def739aece3c112b4d5cccca70487e2343311d72
Waldemar Tomme <waldetomme@gmail.com>
2023-01-23 17:17:19 +0100

    Add demo project

---
+++ Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using DarkLink.Architecture.EventBus;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddEvents();
+
+        services.AddHostedService<Demo>();
+        services.AddSingleton<Demo2>();
+    })
+    .Build()
+    .RunAsync();
+
+internal class Demo : BackgroundService
+{
+    private readonly PublishEventDelegate publishEvent;
+
+    public Demo(PublishEventDelegate publishEvent, Demo2 _)
+    {
+        this.publishEvent = publishEvent;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        publishEvent("hi");
+        return Task.CompletedTask;
+    }
+}
+
+internal class Demo2 : IDisposable
+{
+    private readonly IDisposable subscription;
+
+    public Demo2(EventStream<string> stringEvents, ILogger<Demo2> logger)
+    {
+        subscription = stringEvents
+            .Subscribe(@string => logger.LogInformation("String: {string}", @string));
+    }
+
+    public void Dispose()
+    {
+        subscription.Dispose();
+    }
+}
